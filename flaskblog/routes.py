a46f371dfbac1aefa246a7868ac1df8b117cb692
AniketShahane <40036248+AniketShahane@users.noreply.github.com>
2021-04-28 12:35:34 +0530

    Update routes.py

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,5 +1,5 @@
 import os
-import secrets
+import random
 from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
 from flaskblog import app, db, bcrypt
@@ -59,7 +59,7 @@ def logout():
 
 
 def save_picture(form_picture):
-    random_hex = secrets.token_hex(8)
+    random_hex = str(random.randint(1, 1000000))
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
